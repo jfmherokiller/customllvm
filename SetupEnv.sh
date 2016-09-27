dc5ae253495a78cba408c64986c59c353e43af5c
noah the goodra <peterpan0413@live.com>
2016-09-27 17:17:24 -0500

    working setup

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ev
 export WORKDIR=`pwd`;
-export INSTALLDIR=$WORKDIR\installme
+export INSTALLDIR=$WORKDIR/installme
 mkdir $WORKDIR/llvm-build
 cd $WORKDIR/llvm-build
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly $WORKDIR/llvm
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly -DCMAKE_BUILD_TYPE=Release $WORKDIR/llvm
 ninja install
 tar -zcvf myllvm.tar.gz $INSTALLDIR

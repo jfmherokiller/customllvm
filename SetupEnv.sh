8acd1db18e7ba58db60e98573298b3cb2518ff8c
noah the goodra <peterpan0413@live.com>
2016-09-27 13:09:44 -0500

    travis test

---
+++ SetupEnv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ev
+export WORKDIR=`pwd`;
+export INSTALLDIR=$WORKDIR
+mkdir $WORKDIR/llvm-build
+cd $WORKDIR/llvm-build
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly $WORKDIR/llvm 

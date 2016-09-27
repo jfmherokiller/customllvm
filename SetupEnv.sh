7fcb1d1d9c3aa2c218ad43d168ba210ef0708a2c
noah the goodra <peterpan0413@live.com>
2016-09-27 15:01:52 -0500

    travis release

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -ev
 export WORKDIR=`pwd`;
-export INSTALLDIR=$WORKDIR
+export INSTALLDIR=$WORKDIR\installme
 mkdir $WORKDIR/llvm-build
 cd $WORKDIR/llvm-build
 cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly $WORKDIR/llvm
-ninja
+ninja install
+tar -zcvf myllvm.tar.gz $INSTALLDIR

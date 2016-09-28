e0dd727a0b6d36ea248271825a648d0ed8b5edd2
noah the goodra <peterpan0413@live.com>
2016-09-28 09:45:16 -0500

    added verbose to the ninja part

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -5,7 +5,7 @@ export INSTALLDIR=$WORKDIR/installme
 mkdir $WORKDIR/llvm-build
 cd $WORKDIR/llvm-build
 cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly -DCMAKE_BUILD_TYPE=Release $WORKDIR/llvm
-ninja install
+ninja -v install
 tar -zcvf myllvm.tar.gz $INSTALLDIR
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then mv myllvm.tar.gz myllvm-osx.tar.gz ; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then mv myllvm.tar.gz myllvm-linux.tar.gz ;fi

67c74a089cbfc72c6697df3704de9b04e5e4b241
noah the goodra <peterpan0413@live.com>
2016-09-29 15:54:20 -0500

    more zcpu work

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -4,8 +4,8 @@ export WORKDIR=`pwd`;
 export INSTALLDIR=$WORKDIR/installme
 mkdir $WORKDIR/llvm-build
 cd $WORKDIR/llvm-build
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_CCACHE_BUILD=ON -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=ZCPU -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release $WORKDIR/llvm
-ninja install
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_CCACHE_BUILD=ON -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=ZCPU -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release $WORKDIR/llvm
+make -j2 install
 tar -zcvf myllvm.tar.gz $INSTALLDIR
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then mv myllvm.tar.gz myllvm-osx.tar.gz ; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then mv myllvm.tar.gz myllvm-linux.tar.gz ;fi
---
+++ llvm/lib/Target/ZCPU/ZCPUTargetMachine.cpp
@@ -0,0 +1,18 @@
+//===-- ZCPUTargetMachine.cpp - Define TargetMachine for the ZCPU -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the ZCPU specific subclass of TargetMachine.
+//
+//===----------------------------------------------------------------------===//
+
+#include "ZCPUTargetMachine.h"
+#include "ZCPU.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/Support/TargetRegistry.h"
+using namespace llvm;

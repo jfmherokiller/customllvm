3c278f6a257362564b9c74415c8813d864ac0f72
noah the goodra <peterpan0413@live.com>
2016-09-29 14:50:03 -0500

    blank zcpu target

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -4,7 +4,7 @@ export WORKDIR=`pwd`;
 export INSTALLDIR=$WORKDIR/installme
 mkdir $WORKDIR/llvm-build
 cd $WORKDIR/llvm-build
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release $WORKDIR/llvm
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DLLVM_CCACHE_BUILD=ON -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=ZCPU -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release $WORKDIR/llvm
 ninja install
 tar -zcvf myllvm.tar.gz $INSTALLDIR
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then mv myllvm.tar.gz myllvm-osx.tar.gz ; fi
---
+++ llvm/lib/Target/ZCPU/ZCPU.h
@@ -0,0 +1,23 @@
+//===-- ZCPU.h - Top-level interface for ZCPU representation --*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in the LLVM
+// ZCPU back-end.
+//
+//===----------------------------------------------------------------------===//
+#ifndef LLVM_LIB_TARGET_ZCPU_ZCPU_H
+#define LLVM_LIB_TARGET_ZCPU_ZCPU_H
+
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Target/TargetMachine.h"
+
+
+namespace llvm {
+} // end namespace llvm;
+#endif

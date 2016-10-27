b159d521c72735abb9a1607b1208400cd2775d7b
noah the goodra <peterpan0413@live.com>
2016-10-26 21:19:39 -0500

    werk is beginning

---
+++ llvm/lib/Target/ZCPU/ZCPUTargetMachine.cpp
@@ -0,0 +1,1 @@
+#include "ZCPUTargetMachine.h"
---
+++ llvm/lib/Target/ZCPU/ZCPUTargetMachine.h
@@ -0,0 +1,26 @@
+// WebAssemblyTargetMachine.h - Define TargetMachine for ZCPU -*- C++ -*-
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief This file declares the ZCPU-specific subclass of
+/// TargetMachine.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_ZCPU_ZCPUTARGETMACHINE_H
+#define LLVM_LIB_TARGET_WEBASSEMBLY_WEBASSEMBLYTARGETMACHINE_H
+
+//#include "ZCPUSubtarget.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+
+} // end namespace llvm
+
+#endif
